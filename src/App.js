b842717a1a5715dc9550cf23f6c34b317ef5a6e9
jjcmjavascript <jjcmjavascript@gmail.com>
2022-08-28 22:10:31 -0400

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import React, { useEffect } from 'react';
+import Login from './modules/login';
+import Home from './modules/home';
+import NotFound from './utils/views/404';
+
+import {SessionContext} from './utils/providers/auht'; 
 
 function App() {
+  const {isLoged , setLoged , checkSession } = React.useContext(SessionContext);
+
+  useEffect(()=>{
+    checkSession()
+    .then(res => {
+      setLoged(res);
+    }); 
+  },[]);
+ 
+  const noLogedRoutes = () => {
+    return <>
+      <Route path="/" element={<Login/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </>
+  }
+  
+  const logedRoutes = ()=>{
+    return <>
+        <Route path="/" element={<Home/>}/>
+      </>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          { isLoged ? logedRoutes() : noLogedRoutes()}
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/modules/home/index.jsx
@@ -0,0 +1,10 @@
+import react from 'react'; 
+
+export default function Home(){
+    return <>
+        <h1>
+            Hola BB
+        </h1>
+    </>
+}
+
