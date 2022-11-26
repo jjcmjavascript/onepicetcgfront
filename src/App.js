3818385307110e5e21c163b3d2faf0173e59754e
Junior Corales <jjcmjavascript@gmail.com>
2022-11-26 16:41:51 -0300

    feature: added eslint

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import React, { useEffect } from 'react';
-import Login from './modules/login';
-import Home from './modules/home';
-import NotFound from './views/404';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import Login from "./modules/login";
+import Home from "./modules/home";
+import NotFound from "./views/404";
 
-import {SessionContext} from './providers/sessionProvider'; 
+import { SessionContext } from "./providers/sessionProvider";
 
 function App() {
-  const {isLoged , setLoged , checkSession } = React.useContext(SessionContext);
+  const { isLoged, setLoged, checkSession } = React.useContext(SessionContext);
 
-  useEffect(()=>{
-    checkSession()
-    .then(res => {
+  useEffect(() => {
+    checkSession().then((res) => {
       setLoged(res);
-    }); 
-  },[]);
- 
+    });
+  }, []);
+
   const noLogedRoutes = () => {
     return <>
-      <Route path="/" element={<Login/>}/>
-      <Route path="*" element={<NotFound/>}/>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </>
   }
-  
-  const logedRoutes = ()=>{
+
+  const logedRoutes = () => {
     return <>
-        <Route path="/" element={<Home/>}/>
-      </>
+      <Route path="/" element={<Home />} />
+    </>
   }
 
   return (
-      <BrowserRouter>
-        <Routes>
-          { isLoged ? logedRoutes() : noLogedRoutes()}
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        {isLoged ? logedRoutes() : noLogedRoutes()}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
