6e187dc3a8b17687c20f1afdec85ded7d064f2e0
Junior Corales <jjcmjavascript@gmail.com>
2023-01-18 16:16:24 -0300

    feat: first try to create options in card

---
+++ src/modules/duels/components/cardOptions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CardOptions() {
+  return (
+    <>
+      <div className="hand--options hide">
+        <div className="hand--options__item">Jugar</div>
+        <div className="hand--options__item">Descartar</div>
+        <div className="hand--options__item">Activar</div>
+      </div>
+    </>
+  );
+}
+
+export default CardOptions;
--- src/modules/duels/components/handZone.jsx
+++ src/modules/duels/components/handZone.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import Store from "../provider/duelProvider";
 import FieldCardFull from "./fieldCardFull";
+import CardOptions from "./cardOptions";
 
 function HandZone({ children }) {
   const { states, hooks } = useContext(Store.DuelContext);
   const [hand] = states.hand;
   const [_, setPreview] = states.preview;
+  const [activeCard, setActiveCard] = useState(null);
 
   const onMouseOver = (card) => {
     setPreview(card);
@@ -16,15 +18,40 @@ function HandZone({ children }) {
     setPreview(null);
   };
 
+  const hideOptions = () => {
+    const optionsElement = document.querySelector(".hand--options");
+    optionsElement.classList.add("hide");
+    setActiveCard(null);
+  };
+
+  const toggleOptions = (card) => {
+    hideOptions();
+    const id = `id_${card.uuid}`;
+    const cardHtmlElement = document.getElementById(id);
+    const optionsElement = document.querySelector(".hand--options");
+
+    if (!activeCard || activeCard != card) {
+      setActiveCard(card);
+      optionsElement.style.width = `${cardHtmlElement.clientWidth * 1.5}px`;
+      optionsElement.style.left = `${cardHtmlElement.offsetLeft / 1.05}px`;
+      optionsElement.classList.remove("hide");
+    } else {
+      optionsElement.style.width = `0px`;
+      optionsElement.style.left = `0px`;
+    }
+  };
+
   return (
     <>
       <div className="field--card_area">
+        <CardOptions />
         {hand.map((card) => {
           return (
             <FieldCardFull
               card={card}
               key={card.uuid}
-              onClick={(card) => () => {}}
+              id={`id_${card.uuid}`}
+              onClick={(event) => toggleOptions(card)}
               onMouseOver={() => onMouseOver(card)}
               onMouseOut={() => onMouseOut(card)}
             />
