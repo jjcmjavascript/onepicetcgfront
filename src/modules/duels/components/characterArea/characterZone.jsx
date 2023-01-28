f726b8c8e776eb5c5b6a1e3752be39d4ba3d4ca5
Junior Corales <jjcmjavascript@gmail.com>
2023-01-28 18:43:17 -0300

    feat: character discard and rest

--- src/modules/duels/components/characterArea/characterZone.jsx
+++ src/modules/duels/components/characterArea/characterZone.jsx
@@ -1,13 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 
 import Store from "../../provider/duelProvider";
 import FieldCardFull from "../fieldCardFull";
+import CardOptionsCharacterArea from "./cardOptionsCharacterArea";
+import CardOptionCharacterAreaItem from "./cardOptionCharacterAreaItem";
 
 function CharactedZone({ children }) {
+  const handOptionElementRef = useRef();
   const { states, hooks } = useContext(Store.DuelContext);
   const { boardOne } = states;
+
   const [boardOneState, setBoardOneState] = boardOne;
   const [, setPreview] = states.preview;
+  const [activeCard, setActiveCard] = useState(null);
 
   const onMouseOver = (card) => {
     setPreview(card);
@@ -17,10 +22,61 @@ function CharactedZone({ children }) {
     setPreview(null);
   };
 
+  const toggleOptions = (card) => {
+    const cardHtmlElement = document.querySelector(`#id_${card.uuid}`);
+    const optionsElement = handOptionElementRef.current;
+
+    if (!activeCard || activeCard != card) {
+      setActiveCard(card);
+      optionsElement.style.width = `${cardHtmlElement.clientWidth * 1.5}px`;
+      optionsElement.style.left = `${cardHtmlElement.offsetLeft / 1.05}px`;
+      optionsElement.style.top = `-50px`;
+      optionsElement.classList.remove("hideFull");
+    } else {
+      hideOptions();
+    }
+  };
+
+  const hideOptions = (card) => {
+    const optionsElement = handOptionElementRef.current;
+    optionsElement.classList.add("hideFull");
+    setActiveCard(null);
+  };
+
+  const restCard = () => {
+    const cardHtmlElement = document.querySelector(`#id_${activeCard.uuid}`);
+    cardHtmlElement.classList.toggle("field--card_full_used");
+  };
+
+  const discardCard = () => {
+    setBoardOneState((board)=>{
+      return {
+        ...board,
+        trash: [...board.trash, activeCard],
+        characters: board.characters.filter((card) => card.uuid != activeCard.uuid),
+      }
+    });
+
+    hideOptions();
+  }
+
   return (
     <>
       <div className="field--card_area">
-        {boardOneState.characters.map((card, index) => {
+        <CardOptionsCharacterArea ref={handOptionElementRef}>
+          <CardOptionCharacterAreaItem>Atacar</CardOptionCharacterAreaItem>
+          <CardOptionCharacterAreaItem onClick={restCard}>Rest</CardOptionCharacterAreaItem>
+          <CardOptionCharacterAreaItem>Devolver</CardOptionCharacterAreaItem>
+          <CardOptionCharacterAreaItem onClick={discardCard}>Descartar</CardOptionCharacterAreaItem>
+          <CardOptionCharacterAreaItem>
+            Colocar en Tope
+          </CardOptionCharacterAreaItem>
+          <CardOptionCharacterAreaItem>
+            Colocar en Fondo
+          </CardOptionCharacterAreaItem>
+        </CardOptionsCharacterArea>
+
+        {boardOneState.characters.map((card) => {
           return (
             <FieldCardFull
               id={`id_${card.uuid}`}
@@ -28,6 +84,7 @@ function CharactedZone({ children }) {
               card={card}
               onMouseOut={(_) => onMouseOut(card)}
               onMouseOver={(_) => onMouseOver(card)}
+              onClick={() => toggleOptions(card)}
             />
           );
         })}
