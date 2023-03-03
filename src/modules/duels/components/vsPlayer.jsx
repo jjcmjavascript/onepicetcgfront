6f1f9b35e36a3c2af82e76a28bfe67a41bf54d37
Junior Corales <jjcmjavascript@gmail.com>
2023-03-03 16:35:44 -0300

    feat: respaldo temporal test window

--- src/modules/duels/components/vsPlayer.jsx
+++ src/modules/duels/components/vsPlayer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PreviewAndPhaseZone from "../components/previewAndPhasesArea/previewAndPhaseZone";
 import Board from "../components/v1components/board";
 import EnemyBoard from "../components/v1Enemycomponents/board";
-import BoardV2 from "../components/v2Components/board";
 
 function VsPlayer() {
   return (
